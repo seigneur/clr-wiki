14fbc62dd770c2aaca9d651f8a57d38ba2b47b79
Dias <dias@littlebit.sg>
2024-09-21 20:58:07 +0800

    Add dummy pinata

--- packages/nextjs/utils/pinata.ts
+++ packages/nextjs/utils/pinata.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export async function uploadToPinata(jsonData: any) {
   const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
-  const pinataJWT = process.env.NEXT_PUBLIC_PINATA_JWT || "";
+  const pinataJWT =
+    process.env.NEXT_PUBLIC_PINATA_JWT ||
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI4ZTJkODg2My04NmJjLTQ5NDUtYTc3ZS1hZmFmMzcxNjFiM2QiLCJlbWFpbCI6ImNhc2FhcmxhaUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiYzg4ZDk0YmMzNjU2NjMxYWZmY2IiLCJzY29wZWRLZXlTZWNyZXQiOiI0NjQwMWNiYWYxODZmNTU5MGYzY2VmNWE5Y2MwMmE1OGU2NDU4NWVkYjc4YzA1YWViNDQ1Y2Q1YTIxMjBiMjEwIiwiZXhwIjoxNzU4NDMwMDIyfQ.w3wfKI4Dk2NpgSy_-iQEzI700v_p1eD7FD71kXag0E8";
 
   const { data } = await axios.post(
     url,
